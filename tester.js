e5a1c2b029514d0e82fa4fb801b67b208d176cfe
Don Reniff <donaldreniff@gmail.com>
2024-02-25 14:05:55 -0800

    Add sample data to be sent in request body

--- tester.js
+++ tester.js
@@ -1,1 +1,7 @@
 import http from 'http';
+
+// data to be sent in request body
+const data = JSON.stringify({
+    userEmail: 'userEmail@example.com',
+    purchaseDetails: 'Product: Example Product, Price: $10'
+});
