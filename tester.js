7d9f9665d0f0cbf7f1f8afafb6a2b20def9666df
Don Reniff <donaldreniff@gmail.com>
2024-02-25 14:08:41 -0800

    Set up request options

--- tester.js
+++ tester.js
@@ -5,3 +5,15 @@ const data = JSON.stringify({
     userEmail: 'userEmail@example.com',
     purchaseDetails: 'Product: Example Product, Price: $10'
 });
+
+// options for HTTP request, per http documentation
+const options = {
+    hostname: 'localhost',
+    port: 3000, // if the microservice is running on port 3000 locally
+    path: '/api/sendEmail',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+    },
+};
