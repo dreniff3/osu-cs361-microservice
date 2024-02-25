ceb653fa1948cf005878e3724b5a39417c1eca8d
Don Reniff <donaldreniff@gmail.com>
2024-02-25 13:52:18 -0800

    Set up endpoint for receiving purchase notifications

--- emailService.js
+++ emailService.js
@@ -2,3 +2,22 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import nodemailer from 'nodemailer';
+
+const app = express();
+
+// middleware for parsing JSON bodies
+app.use(bodyParser.json());
+
+// endpoint for receiving purchase notifications
+app.post('/api/sendEmail', (req, res) => {
+    const { userEmail, purchaseDetails } = req.body;
+
+    // create SMTP transporter object per nodemailer documentation
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'sender@example.com',
+            pass: 'example_password'
+        },
+    });
+});
